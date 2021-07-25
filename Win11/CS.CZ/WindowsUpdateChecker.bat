190cd54bd969d5255960cc3387945ad31521df4a
GD Matto58 <mattothepou@gmail.com>
2021-07-25 17:50:12 +0200

    Added Windows 11/ Přidán Windows 11

---
+++ Win11/CS.CZ/WindowsUpdateChecker.bat
@@ -0,0 +1,313 @@
+@echo off
+title Windows Update Checker pro Windows 11
+echo Hledani novych aktualizaci...
+timeout 27 >nul
+echo.
+echo Aktualizace nalezena! Stisknete jakoukoliv klavesu, abyste zahajil aktualizaci.
+pause >nul
+cls
+title Probiha aktualizace...
+echo Probiha aktualizace... 0%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 1%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 2%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 3%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 4%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 5%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 6%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 8%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 9%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 10%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 11%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 12%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 13%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 14%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 15%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 16%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 17%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 18%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 19%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 20%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 21%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 22%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 23%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 24%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 25%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 26%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 27%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 28%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 29%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 30%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 31%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 32%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 33%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 34%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 35%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 36%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 37%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 38%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 39%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 40%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 41%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 42%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 43%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 44%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 45%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 46%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 47%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 48%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 49%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 50%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 51%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 52%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 53%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 54%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 55%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 56%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 57%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 58%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 59%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 60%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 61%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 62%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 63%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 64%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 65%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 66%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 67%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 68%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 69%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 70%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 71%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 72%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 73%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 74%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 75%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 76%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 77%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 78%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 79%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 80%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 81%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 82%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 83%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 84%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 85%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 86%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 87%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 88%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 89%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 90%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 91%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 92%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 93%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 94%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 95%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 96%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 97%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 98%%
+timeout 8 >nul
+cls
+echo Probiha aktualizace... 99%%
+timeout 17 >nul
+title Kriticka chyba
+echo Nastala kriticka chyba a Windows Update Checker nemohl aktualizovat na nejnovejsi verzi Windows 11.
+echo Stisknete jakoukoli klavesu, abyste ukoncili Windows Update Checker.
+pause >nul
+exit
+
+:WinUpdateCheck_Quit
+exit
---
+++ Win11/EN.US/WindowsUpdateChecker.bat
@@ -0,0 +1,310 @@
+@echo off
+title Windows Update Checker for Windows 11
+echo Searching for updates...
+timeout 27 >nul
+echo.
+echo Update found! Press any key to start downloading.
+pause >nul
+cls
+title Updating...
+echo Updating... 0%%
+timeout 8 >nul
+cls
+echo Updating... 1%%
+timeout 8 >nul
+cls
+echo Updating... 2%%
+timeout 8 >nul
+cls
+echo Updating... 3%%
+timeout 8 >nul
+cls
+echo Updating... 4%%
+timeout 8 >nul
+cls
+echo Updating... 5%%
+timeout 8 >nul
+cls
+echo Updating... 6%%
+timeout 8 >nul
+cls
+echo Updating... 8%%
+timeout 8 >nul
+cls
+echo Updating... 9%%
+timeout 8 >nul
+cls
+echo Updating... 10%%
+timeout 8 >nul
+cls
+echo Updating... 11%%
+timeout 8 >nul
+cls
+echo Updating... 12%%
+timeout 8 >nul
+cls
+echo Updating... 13%%
+timeout 8 >nul
+cls
+echo Updating... 14%%
+timeout 8 >nul
+cls
+echo Updating... 15%%
+timeout 8 >nul
+cls
+echo Updating... 16%%
+timeout 8 >nul
+cls
+echo Updating... 17%%
+timeout 8 >nul
+cls
+echo Updating... 18%%
+timeout 8 >nul
+cls
+echo Updating... 19%%
+timeout 8 >nul
+cls
+echo Updating... 20%%
+timeout 8 >nul
+cls
+echo Updating... 21%%
+timeout 8 >nul
+cls
+echo Updating... 22%%
+timeout 8 >nul
+cls
+echo Updating... 23%%
+timeout 8 >nul
+cls
+echo Updating... 24%%
+timeout 8 >nul
+cls
+echo Updating... 25%%
+timeout 8 >nul
+cls
+echo Updating... 26%%
+timeout 8 >nul
+cls
+echo Updating... 27%%
+timeout 8 >nul
+cls
+echo Updating... 28%%
+timeout 8 >nul
+cls
+echo Updating... 29%%
+timeout 8 >nul
+cls
+echo Updating... 30%%
+timeout 8 >nul
+cls
+echo Updating... 31%%
+timeout 8 >nul
+cls
+echo Updating... 32%%
+timeout 8 >nul
+cls
+echo Updating... 33%%
+timeout 8 >nul
+cls
+echo Updating... 34%%
+timeout 8 >nul
+cls
+echo Updating... 35%%
+timeout 8 >nul
+cls
+echo Updating... 36%%
+timeout 8 >nul
+cls
+echo Updating... 37%%
+timeout 8 >nul
+cls
+echo Updating... 38%%
+timeout 8 >nul
+cls
+echo Updating... 39%%
+timeout 8 >nul
+cls
+echo Updating... 40%%
+timeout 8 >nul
+cls
+echo Updating... 41%%
+timeout 8 >nul
+cls
+echo Updating... 42%%
+timeout 8 >nul
+cls
+echo Updating... 43%%
+timeout 8 >nul
+cls
+echo Updating... 44%%
+timeout 8 >nul
+cls
+echo Updating... 45%%
+timeout 8 >nul
+cls
+echo Updating... 46%%
+timeout 8 >nul
+cls
+echo Updating... 47%%
+timeout 8 >nul
+cls
+echo Updating... 48%%
+timeout 8 >nul
+cls
+echo Updating... 49%%
+timeout 8 >nul
+cls
+echo Updating... 50%%
+timeout 8 >nul
+cls
+echo Updating... 51%%
+timeout 8 >nul
+cls
+echo Updating... 52%%
+timeout 8 >nul
+cls
+echo Updating... 53%%
+timeout 8 >nul
+cls
+echo Updating... 54%%
+timeout 8 >nul
+cls
+echo Updating... 55%%
+timeout 8 >nul
+cls
+echo Updating... 56%%
+timeout 8 >nul
+cls
+echo Updating... 57%%
+timeout 8 >nul
+cls
+echo Updating... 58%%
+timeout 8 >nul
+cls
+echo Updating... 59%%
+timeout 8 >nul
+cls
+echo Updating... 60%%
+timeout 8 >nul
+cls
+echo Updating... 61%%
+timeout 8 >nul
+cls
+echo Updating... 62%%
+timeout 8 >nul
+cls
+echo Updating... 63%%
+timeout 8 >nul
+cls
+echo Updating... 64%%
+timeout 8 >nul
+cls
+echo Updating... 65%%
+timeout 8 >nul
+cls
+echo Updating... 66%%
+timeout 8 >nul
+cls
+echo Updating... 67%%
+timeout 8 >nul
+cls
+echo Updating... 68%%
+timeout 8 >nul
+cls
+echo Updating... 69%%
+timeout 8 >nul
+cls
+echo Updating... 70%%
+timeout 8 >nul
+cls
+echo Updating... 71%%
+timeout 8 >nul
+cls
+echo Updating... 72%%
+timeout 8 >nul
+cls
+echo Updating... 73%%
+timeout 8 >nul
+cls
+echo Updating... 74%%
+timeout 8 >nul
+cls
+echo Updating... 75%%
+timeout 8 >nul
+cls
+echo Updating... 76%%
+timeout 8 >nul
+cls
+echo Updating... 77%%
+timeout 8 >nul
+cls
+echo Updating... 78%%
+timeout 8 >nul
+cls
+echo Updating... 79%%
+timeout 8 >nul
+cls
+echo Updating... 80%%
+timeout 8 >nul
+cls
+echo Updating... 81%%
+timeout 8 >nul
+cls
+echo Updating... 82%%
+timeout 8 >nul
+cls
+echo Updating... 83%%
+timeout 8 >nul
+cls
+echo Updating... 84%%
+timeout 8 >nul
+cls
+echo Updating... 85%%
+timeout 8 >nul
+cls
+echo Updating... 86%%
+timeout 8 >nul
+cls
+echo Updating... 87%%
+timeout 8 >nul
+cls
+echo Updating... 88%%
+timeout 8 >nul
+cls
+echo Updating... 89%%
+timeout 8 >nul
+cls
+echo Updating... 90%%
+timeout 8 >nul
+cls
+echo Updating... 91%%
+timeout 8 >nul
+cls
+echo Updating... 92%%
+timeout 8 >nul
+cls
+echo Updating... 93%%
+timeout 8 >nul
+cls
+echo Updating... 94%%
+timeout 8 >nul
+cls
+echo Updating... 95%%
+timeout 8 >nul
+cls
+echo Updating... 96%%
+timeout 8 >nul
+cls
+echo Updating... 97%%
+timeout 8 >nul
+cls
+echo Updating... 98%%
+timeout 8 >nul
+cls
+echo Updating... 99%%
+timeout 17 >nul
+title Critical error
+echo An critical error has occured and Windows Update Checker could not update to the newest version of Windows 11.
+echo Press any key to close Windows Update Checker.
+pause >nul
+exit
